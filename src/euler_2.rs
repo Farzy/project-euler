ef0ed7824238230f5f703c18b3ef53062d980b60
Farzad FARID <farzy@farzy.org>
2020-07-17 22:53:03 +0200

    Change Euler 2 to proposed solution

--- src/euler_2.rs
+++ src/euler_2.rs
@@ -2,19 +2,13 @@ const MAX: u32 = 4_000_000;
 
 pub fn main() -> String {
     let mut sum = 0;
-    // Let's start at i = 2 because it's the first non-null even number to sum
-    let (mut i, mut j) = (2u32, 3u32);
-    let mut temp;
-    while i <= MAX {
-        sum += i;
-        // Every 1 out of 3 fibonacci numbers is even
-        for _ in 1..=3 {
-            // Maybe one day Rust will support destructuring assignment like Python…
-            temp = i;
-            i = j;
-            j = temp + j;
-            // println!("i: {}, sum: {}", temp, sum);
-        }
+    let (mut a, mut b) = (1u32, 1u32);
+    let mut c = a + b;
+    while c <= MAX {
+        sum += c;
+        a = b + c;
+        b = a + c;
+        c = a + b;
     }
     format!("{}", sum)
 }
